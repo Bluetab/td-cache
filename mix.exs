1838a023004ffb65135c734260be95ffc1e3671a
sc <sonia.curiel@bluetab.net>
2019-08-01 09:02:52 +0200

    chore: [TD-2009] upgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCache.MixProject do
   def project do
     [
       app: :td_cache,
-      version: "3.3.1",
+      version: "3.3.3",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
