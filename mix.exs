72dade41739d988daa2d1cb0963b3609fd2fcedd
Tom Crossland <tom.crossland@bluetab.net>
2021-07-26 16:24:56 +0200

    fix: remove entry from user_name_to_id when deleting a user

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCache.MixProject do
   def project do
     [
       app: :td_cache,
-      version: "4.25.3",
+      version: "4.26.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
