1479675c7b1dc52fae4c40c74fd76efbd37f8c43
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2023-08-03 15:30:09 +0200

    feat: TD-5891 Cache i18n Messages

---
+++ lib/td_cache/i18n_cache.ex
@@ -0,0 +1,157 @@
+defmodule TdCache.I18nCache do
+  @moduledoc """
+  Shared cache for i18n.
+  """
+  use GenServer
+
+  alias TdCache.Redix
+  @i18n_key :i18n
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def put(locale, message, opts \\ []) do
+    GenServer.call(__MODULE__, {:put, locale, message, opts})
+  end
+
+  def delete(locale) do
+    GenServer.call(__MODULE__, {:delete, locale})
+  end
+
+  def get_definition(locale, key, opts \\ [])
+
+  def get_definition(_locale, "i18n:" <> _ = definition_key, opts) do
+    response =
+      case get_cache(definition_key, fn -> read_definition(definition_key) end, opts) do
+        nil ->
+          nil
+
+        message ->
+          message
+      end
+
+    {:ok, response}
+  end
+
+  def get_definition(locale, message_id, opts) do
+    get_definition(locale, i18n_definition_key(locale, message_id), opts)
+  end
+
+  def list_by_locale(locale) do
+    key = i18n_locale_key(locale)
+
+    case(Redix.command(["SMEMBERS", key])) do
+      {:ok, ids} ->
+        Enum.map(ids, &read_definition(&1))
+
+      _ ->
+        []
+    end
+  end
+
+  def map_keys_by_prefix(locale, prefix) do
+    pattern = i18n_definition_key(locale, prefix <> "*")
+
+    {:ok, keys} = Redix.command(["KEYS", pattern])
+
+    keys
+    |> Enum.into(%{}, fn definition_key ->
+      {defition_key_to_message_id(locale, definition_key), read_definition(definition_key)}
+    end)
+  end
+
+  ## Callbacks
+
+  @impl true
+  def init(_args) do
+    {:ok, nil}
+  end
+
+  @impl true
+
+  def handle_call({:put, locale, message, opts}, _from, state) do
+    reply = put_message(locale, message, opts)
+
+    {:reply, reply, state}
+  end
+
+  def handle_call({:delete, locale}, _from, state) do
+    reply = delete_locale(locale)
+    {:reply, reply, state}
+  end
+
+  ## Private Functions
+  defp get_cache(key, fun, opts) do
+    if Keyword.get(opts, :refresh, false) do
+      message = fun.()
+      ConCache.put(@i18n_key, key, message)
+      message
+    else
+      ConCache.get_or_store(@i18n_key, key, fn -> fun.() end)
+    end
+  end
+
+  defp delete_cache(key) do
+    ConCache.delete(@i18n_key, key)
+  end
+
+  defp read_definition(definition_key) do
+    case Redix.command(["GET", definition_key]) do
+      {:ok, nil} ->
+        nil
+
+      {:ok, message} ->
+        message
+    end
+  end
+
+  defp delete_locale(locale) do
+    locale_key = i18n_locale_key(locale)
+
+    keys_to_delete =
+      case(Redix.command(["SMEMBERS", locale_key])) do
+        {:ok, ids} -> ids
+        _ -> []
+      end
+
+    response =
+      keys_to_delete
+      |> Enum.chunk_every(1000)
+      |> Enum.map(&["DEL" | &1])
+      |> Kernel.++([["DEL", locale_key]])
+      |> Redix.transaction_pipeline()
+
+    Enum.each(keys_to_delete, &delete_cache(&1))
+    response
+  end
+
+  defp put_message(locale, {message_id, definition} = _message, _opts) do
+    locale_key = i18n_locale_key(locale)
+    definition_key = i18n_definition_key(locale, message_id)
+
+    commands = [
+      ["SET", definition_key, definition],
+      ["SADD", locale_key, definition_key]
+    ]
+
+    results = Redix.transaction_pipeline(commands)
+    :ok = delete_cache(definition_key)
+    results
+  end
+
+  defp i18n_locale_key(locale), do: "#{@i18n_key}:keys:#{locale}"
+
+  defp i18n_definition_key(locale, message_id), do: "#{@i18n_key}:#{locale}:#{message_id}"
+
+  defp defition_key_to_message_id(locale, definition_key) when is_atom(locale),
+    do: defition_key_to_message_id(Atom.to_string(locale), definition_key)
+
+  defp defition_key_to_message_id(locale, "i18n:" <> locale_message_id) do
+    [locale_message, message_id] = String.split(locale_message_id, ":")
+
+    if locale == locale_message, do: message_id, else: nil
+  end
+end
---
+++ test/td_cache/i18n_cache_test.exs
@@ -0,0 +1,122 @@
+defmodule TdCache.I18nCacheTest do
+  use ExUnit.Case
+
+  import TdCache.TestOperators
+
+  alias TdCache.I18nCache
+
+  @locales [:en]
+
+  doctest TdCache.I18nCache
+
+  setup do
+    [messages_en] = Enum.map(@locales, &get_messages(&1))
+
+    on_exit(fn ->
+      Enum.map(@locales, &I18nCache.delete(&1))
+    end)
+
+    [messages_en: {:en, messages_en}]
+  end
+
+  test "put/2 returns Ok", %{messages_en: {locale, messages_en}} do
+    Enum.each(messages_en, fn message ->
+      assert {:ok, ["OK", 1]} = I18nCache.put(locale, message)
+    end)
+  end
+
+  test "put/2 returns updates only when updated at is changed", %{
+    messages_en: {locale, messages_en}
+  } do
+    put_messages(locale, messages_en)
+
+    {message_id, definition} = get_ramdom_message(messages_en)
+
+    new_definition = definition <> "_updated"
+
+    assert {:ok, _} = I18nCache.put(locale, {message_id, new_definition})
+
+    assert {:ok, ^new_definition} = I18nCache.get_definition(locale, message_id)
+  end
+
+  test "put/2 returns ok adding new message", %{messages_en: {locale, messages_en}} do
+    put_messages(locale, messages_en)
+
+    assert {:ok, ["OK", 1]} = I18nCache.put(locale, {"dx.dax", "dax_en"})
+
+    assert length(I18nCache.list_by_locale(locale)) == 6
+  end
+
+  test "delete/1 return ok when keys and messages was deleted", %{
+    messages_en: {locale, messages_en}
+  } do
+    put_messages(locale, messages_en)
+
+    assert {:ok, [5, 1]} = I18nCache.delete(locale)
+
+    assert length(I18nCache.list_by_locale(locale)) == 0
+  end
+
+  test "get_definition/2 return definition from message_id and locale", %{
+    messages_en: {locale, messages_en}
+  } do
+    put_messages(locale, messages_en)
+    {message_id, definition} = get_ramdom_message(messages_en)
+
+    assert {:ok, ^definition} = I18nCache.get_definition(locale, message_id)
+  end
+
+  test "get_definition/2 return definition from message_key and locale", %{
+    messages_en: {locale, messages_en}
+  } do
+    put_messages(locale, messages_en)
+    {message_id, definition} = get_ramdom_message(messages_en)
+
+    assert {:ok, ^definition} = I18nCache.get_definition(locale, "i18n:#{locale}:#{message_id}")
+  end
+
+  test "list_by_locale/1 return the list of definitions by locale", %{
+    messages_en: {locale, messages_en}
+  } do
+    put_messages(locale, messages_en)
+
+    messages = Enum.map(messages_en, fn {_, message} -> message end)
+
+    assert messages <|> I18nCache.list_by_locale(locale)
+  end
+
+  test "map_keys_by_prefix/2 ", %{messages_en: {locale, messages_en}} do
+    messages = %{
+      "kr.kar" => "tar#{locale}",
+      "kr.kar.taaar" => "taaar_#{locale}",
+      "kr.kar.teeer" => "teeer_#{locale}",
+      "kr.kar.tiiir" => "tiiir_#{locale}"
+    }
+
+    messages
+    |> Map.merge(messages_en)
+    |> (&put_messages(locale, &1)).()
+
+    prefix = "kr.kar"
+
+    assert messages <~> I18nCache.map_keys_by_prefix(locale, prefix)
+  end
+
+  defp put_messages(locale, messages) do
+    Enum.each(messages, &I18nCache.put(locale, &1))
+  end
+
+  defp get_messages(locale) do
+    %{
+      "br.bar" => "bar_#{locale}",
+      "fo.foo" => "foo_#{locale}",
+      "bz.baz" => "faz_#{locale}",
+      "yz.yxz" => "yxz_#{locale}",
+      "qx.qux" => "qux_#{locale}"
+    }
+  end
+
+  defp get_ramdom_message(messages) do
+    Enum.at(messages, Enum.random(0..(Enum.count(messages) - 1)))
+  end
+end
