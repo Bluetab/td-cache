fcafe22201394173cf1dd747aa358c0b41737e29
Jandro Oliva <alejandro.oliva@bluetab.net>
2024-01-26 02:02:55 +0100

    WIP: TD-6258 resource_content_cache

--- lib/td_cache/resource_content_cache.ex
+++ lib/td_cache/resource_content_cache.ex
@@ -6,205 +6,107 @@ defmodule TdCache.ResourceContentCache do
   alias TdCache.Redix
   @i18n_resource_key :i18n_resource
 
-  def put(resource_content, opts \\ []) do
-    put_resource_content(resource_content, opts)
-  end
-
-  defp put_resource_content(
-         %{
-           #  id: id,
-           locale: %{lang: lang},
-           resource_type: resource_type,
-           resource_id: resource_id,
-           content: content
-           #  updated_at: updated_at
-         },
-         _opts
-       ) do
-    IO.inspect(content, label: "content --->")
-
+  def put(
+        %{
+          locale: %{lang: lang},
+          resource_type: resource_type,
+          resource_id: resource_id,
+          content: content
+        },
+        _opts \\ []
+      ) do
     {content_keys, i18n_resource_keys} =
-      Enum.reduce(content, {[], []}, fn {key, value}, {ckeys, rkeys} ->
-        content_key = i18n_content_key(lang, resource_type, resource_id, key)
+      Enum.reduce(content, {[], []}, fn {field, value}, {ckeys, rkeys} ->
+        content_key = i18n_content_key(resource_type, resource_id, lang, field)
 
         {ckeys ++ [content_key, value], [content_key | rkeys]}
       end)
-      |> IO.inspect(label: "result reduce --->")
 
-    commands = [["MSET", content_keys]]
+    i18n_resource_key = i18n_resource_key(resource_type)
 
-    # i18n_lang_resource_key = i18n_lang_resource_key(lang, resource_type)
+    delete_commands = delete_commands(resource_type, resource_id, lang)
+
+    commands =
+      delete_commands ++
+        [
+          ["MSET" | content_keys],
+          ["SADD", i18n_resource_key | i18n_resource_keys]
+        ]
+
     Redix.transaction_pipeline(commands)
-    # |> IO.inspect()
   end
 
-  defp i18n_resource_key(lang, resource_type),
-    do: "#{@i18n_resource_key}:#{resource_type}:keys:#{lang}"
+  def get(resource_type, resource_id, lang, field) do
+    field_key = i18n_content_key(resource_type, resource_id, lang, field)
 
-  defp i18n_content_key(lang, resource_type, resource_id, key),
-    do: "#{@i18n_resource_key}:#{resource_type}:#{resource_id}:#{lang}:#{key}"
+    case Redix.command(["GET", field_key]) do
+      {:ok, nil} ->
+        nil
 
-  # def get_definition(lang, key, opts \\ [])
+      {:ok, field_value} ->
+        field_value
+    end
+  end
 
-  # def get_definition(_lang, "i18n:" <> _ = definition_key, opts) do
-  #   case get_cache(definition_key, fn -> read_definition(definition_key) end, opts) do
-  #     nil ->
-  #       nil
+  def get_resource_keys(resource_type) do
+    resource_key = i18n_resource_key(resource_type)
 
-  #     message ->
-  #       message
-  #   end
-  # end
+    case Redix.command(["SMEMBERS", resource_key]) do
+      {:ok, nil} ->
+        nil
 
-  # def get_definition(lang, message_id, opts) do
-  #   case get_definition(lang, i18n_definition_key(lang, message_id), opts) do
-  #     nil ->
-  #       Keyword.get(opts, :default_value)
+      {:ok, field_value} ->
+        field_value
+    end
+  end
 
-  #     definition ->
-  #       definition
-  #   end
-  # end
+  def delete(resource_type) do
+    delete_commands = delete_commands(resource_type, "*", "*")
 
-  # def get_definitions_by_value(value, lang, opts \\ [])
+    case delete_commands do
+      [] -> []
+      [_ | _] -> Redix.transaction_pipeline(delete_commands)
+    end
+  end
 
-  # def get_definitions_by_value(value, lang, opts) do
-  #   prefix = Keyword.get(opts, :prefix, "")
+  def delete(resource_type, resource_id) do
+    delete_commands = delete_commands(resource_type, resource_id, "*")
 
-  #   lang
-  #   |> map_keys_by_prefix(prefix)
-  #   |> Enum.filter(fn {_, v} -> v == value end)
-  #   |> Enum.map(fn {message_id, definition} ->
-  #     %{definition: definition, message_id: message_id}
-  #   end)
-  # end
+    case delete_commands do
+      [] -> []
+      [_ | _] -> Redix.transaction_pipeline(delete_commands)
+    end
+  end
 
-  # def list_by_lang(lang) do
-  #   key = i18n_lang_key(lang)
+  def delete(resource_type, resource_id, lang) do
+    delete_commands = delete_commands(resource_type, resource_id, lang)
 
-  #   case(Redix.command(["SMEMBERS", key])) do
-  #     {:ok, ids} ->
-  #       Enum.map(ids, &read_definition(&1))
+    case delete_commands do
+      [] -> []
+      [_ | _] -> Redix.transaction_pipeline(delete_commands)
+    end
+  end
 
-  #     _ ->
-  #       []
-  #   end
-  # end
+  def delete_commands(resource_type, resource_id, lang) do
+    i18n_resource_key = i18n_resource_key(resource_type)
 
-  # def map_keys_by_prefix(lang, prefix) do
-  #   pattern = i18n_definition_key(lang, prefix <> "*")
+    i18n_content_lang_key = i18n_content_lang_key(resource_type, resource_id, lang)
 
-  #   {:ok, keys} = Redix.command(["KEYS", pattern])
+    case Redix.command(["KEYS", i18n_content_lang_key <> ":*"]) do
+      {:ok, []} ->
+        []
 
-  #   Enum.into(keys, %{}, fn definition_key ->
-  #     {defition_key_to_message_id(lang, definition_key), read_definition(definition_key)}
-  #   end)
-  # end
+      {:ok, redis_key_fields} ->
+        [["DEL" | redis_key_fields], ["SREM", i18n_resource_key | redis_key_fields]]
+    end
+  end
 
-  # ## Callbacks
+  defp i18n_resource_key(resource_type),
+    do: "#{@i18n_resource_key}:#{resource_type}:keys"
 
-  # @impl true
-  # def init(_args) do
-  #   {:ok, nil}
-  # end
+  defp i18n_content_key(resource_type, resource_id, lang, field),
+    do: "#{@i18n_resource_key}:#{resource_type}:#{resource_id}:#{lang}:#{field}"
 
-  # @impl true
-  # def handle_call({:put, lang, message, opts}, _from, state) do
-  #   reply = put_message(lang, message, opts)
-
-  #   {:reply, reply, state}
-  # end
-
-  # @impl true
-  # def handle_call({:delete, lang}, _from, state) do
-  #   reply = delete_lang(lang)
-  #   {:reply, reply, state}
-  # end
-
-  # @impl true
-  # def handle_call({:delete, lang, message_id}, _from, state) do
-  #   reply = delete_message(lang, message_id)
-  #   {:reply, reply, state}
-  # end
-
-  # ## Private Functions
-  # defp get_cache(key, fun, opts) do
-  #   if Keyword.get(opts, :refresh, false) do
-  #     message = fun.()
-  #     ConCache.put(@i18n_resources_key, key, message)
-  #     message
-  #   else
-  #     ConCache.get_or_store(@i18n_resources_key, key, fn -> fun.() end)
-  #   end
-  # end
-
-  # defp delete_cache(key) do
-  #   ConCache.delete(@i18n_resources_key, key)
-  # end
-
-  # defp read_definition(definition_key) do
-  #   case Redix.command(["GET", definition_key]) do
-  #     {:ok, nil} ->
-  #       nil
-
-  #     {:ok, message} ->
-  #       message
-  #   end
-  # end
-
-  # defp delete_lang(lang) do
-  #   lang_key = i18n_lang_key(lang)
-
-  #   keys_to_delete =
-  #     case(Redix.command(["SMEMBERS", lang_key])) do
-  #       {:ok, ids} -> ids
-  #       _ -> []
-  #     end
-
-  #   response =
-  #     keys_to_delete
-  #     |> Enum.chunk_every(1000)
-  #     |> Enum.map(&["DEL" | &1])
-  #     |> Kernel.++([["DEL", lang_key]])
-  #     |> Redix.transaction_pipeline()
-
-  #   Enum.each(keys_to_delete, &delete_cache(&1))
-  #   response
-  # end
-
-  # defp delete_message(lang, message_id) do
-  #   definition_key = i18n_definition_key(lang, message_id)
-  #   commands = ["DEL", definition_key]
-
-  #   response = Redix.command(commands)
-  #   :ok = delete_cache(definition_key)
-  #   response
-  # end
-
-  # defp put_message(lang, %{message_id: message_id, definition: definition} = _message, _opts) do
-  #   lang_key = i18n_lang_key(lang)
-  #   definition_key = i18n_definition_key(lang, message_id)
-
-  #   commands = [
-  #     ["SET", definition_key, definition],
-  #     ["SADD", lang_key, definition_key]
-  #   ]
-
-  #   response = Redix.transaction_pipeline(commands)
-  #   :ok = delete_cache(definition_key)
-  #   response
-  # end
-
-  # defp i18n_lang_key(lang), do: "#{@i18n_resources_key}:keys:#{lang}"
-
-  # defp i18n_definition_key(lang, message_id), do: "#{@i18n_resources_key}:#{lang}:#{message_id}"
-
-  # defp defition_key_to_message_id(lang, definition_key) when is_atom(lang),
-  #   do: defition_key_to_message_id(Atom.to_string(lang), definition_key)
-
-  # defp defition_key_to_message_id(lang, "i18n:" <> lang_message_id) do
-  #   [lang_message, message_id] = String.split(lang_message_id, ":")
-
-  #   if lang == lang_message, do: message_id, else: nil
-  # end
+  defp i18n_content_lang_key(resource_type, resource_id, lang),
+    do: "#{@i18n_resource_key}:#{resource_type}:#{resource_id}:#{lang}"
 end
--- test/td_cache/resource_content_cache_test.exs
+++ test/td_cache/resource_content_cache_test.exs
@@ -4,24 +4,167 @@ defmodule TdCache.ResourceContentCacheTest do
 
   setup do
     content = %{"field1" => "var1", "field2" => "var2", "field3" => "var3"}
+    resource_type = "res_type"
+    lang = "es"
+    resource_id = 1000
 
     resource_content = %{
-      locale: %{lang: "es"},
-      resource_type: "res_type",
-      resource_id: 1000,
+      locale: %{lang: lang},
+      resource_type: resource_type,
+      resource_id: resource_id,
       content: content
     }
 
-    [resource_content: resource_content]
-    ## Hacer el on exit con los argumentos especificos del borrado
-    ## ResourceContentCache.delete(...)
+    on_exit(fn ->
+      nil
+      ResourceContentCache.delete(resource_type)
+    end)
+
+    [
+      resource_content: resource_content,
+      content: content,
+      resource_type: resource_type,
+      lang: lang,
+      resource_id: resource_id
+    ]
   end
 
   describe "resourceContentCache" do
-    test "write a i18n_resource content and reads it back", %{resource_content: resource_content} do
-      IO.inspect(resource_content, label: "resource_content ->")
+    test "put i18n_resource content", %{resource_content: resource_content} do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
 
-      ResourceContentCache.put(resource_content)
+      # assert {:ok, ["OK", 3]} =
+      #          ResourceContentCache.put(generate_resource_content("my_type", 1001, "fr"))
+
+      # assert {:ok, ["OK", 3]} =
+      #          ResourceContentCache.put(generate_resource_content("res_type", 1000, "fr"))
+
+      # assert {:ok, ["OK", 3]} =
+      #          ResourceContentCache.put(generate_resource_content("res_type", 1001, "es"))
+
+      # assert {:ok, ["OK", 3]} =
+      #          ResourceContentCache.put(generate_resource_content("my_type", 1000, "es"))
+
+      assert "var1" == ResourceContentCache.get("res_type", 1000, "es", "field1")
+      assert "var2" == ResourceContentCache.get("res_type", 1000, "es", "field2")
+      assert "var3" == ResourceContentCache.get("res_type", 1000, "es", "field3")
+
+      resource_keys_list = ResourceContentCache.get_resource_keys("res_type")
+      assert length(resource_keys_list) == 3
+
+      assert resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field1" end)
+
+      assert resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field2" end)
+
+      assert resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field3" end)
     end
+
+    test "get value on found i18n_resource content", %{resource_content: resource_content} do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
+      assert "var1" == ResourceContentCache.get("res_type", 1000, "es", "field1")
+    end
+
+    test "get error on not found i18n_resource content", %{resource_content: resource_content} do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field4")
+    end
+
+    test "update value for i18n_resource content", %{resource_content: resource_content} do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
+      assert "var1" == ResourceContentCache.get("res_type", 1000, "es", "field1")
+      assert "var2" == ResourceContentCache.get("res_type", 1000, "es", "field2")
+      assert "var3" == ResourceContentCache.get("res_type", 1000, "es", "field3")
+
+      updated_field_value =
+        resource_content
+        |> Map.get(:content)
+        |> Map.drop(["field3"])
+        |> Map.put("field4", "var4")
+        |> Map.put("field1", "var5")
+
+      update_resource_content = Map.put(resource_content, :content, updated_field_value)
+
+      assert {:ok, [3, 3, "OK", 3]} = ResourceContentCache.put(update_resource_content)
+
+      assert "var5" == ResourceContentCache.get("res_type", 1000, "es", "field1")
+      assert "var2" == ResourceContentCache.get("res_type", 1000, "es", "field2")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field3")
+      assert "var4" == ResourceContentCache.get("res_type", 1000, "es", "field4")
+
+      resource_keys_list = ResourceContentCache.get_resource_keys("res_type")
+      assert length(resource_keys_list) == 3
+
+      assert resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field1" end)
+
+      assert resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field2" end)
+
+      assert resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field4" end)
+
+      refute resource_keys_list
+             |> Enum.any?(fn value -> value == "i18n_resource:res_type:1000:es:field3" end)
+    end
+
+    test "deletes resource type", %{
+      resource_content: resource_content,
+      resource_type: resource_type
+    } do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
+
+      assert {:ok, [3, 3]} = ResourceContentCache.delete(resource_type)
+
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field1")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field2")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field3")
+
+      assert [] = ResourceContentCache.get_resource_keys("res_type")
+    end
+
+    test "deletes resource", %{
+      resource_content: resource_content,
+      resource_type: resource_type,
+      resource_id: resource_id
+    } do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
+
+      assert {:ok, [3, 3]} = ResourceContentCache.delete(resource_type, resource_id)
+
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field1")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field2")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field3")
+
+      assert [] = ResourceContentCache.get_resource_keys("res_type")
+    end
+
+    test "deletes resource lang", %{
+      resource_content: resource_content,
+      resource_type: resource_type,
+      resource_id: resource_id,
+      lang: lang
+    } do
+      assert {:ok, ["OK", 3]} = ResourceContentCache.put(resource_content)
+
+      assert {:ok, [3, 3]} = ResourceContentCache.delete(resource_type, resource_id, lang)
+
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field1")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field2")
+      assert nil == ResourceContentCache.get("res_type", 1000, "es", "field3")
+
+      assert [] = ResourceContentCache.get_resource_keys("res_type")
+    end
+  end
+
+  defp generate_resource_content(resource_type, resource_id, lang) do
+    resource_content = %{
+      locale: %{lang: lang},
+      resource_type: resource_type,
+      resource_id: resource_id,
+      content: %{"field1" => "var1", "field2" => "var2", "field3" => "var3"}
+    }
   end
 end
