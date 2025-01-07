d2e453530ff988cbb3d9840302d8e64e9539af32
saul.isern <saul.isern@bluetab.net>
2025-01-07 16:36:46 +0100

    chore: bump versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCache.MixProject do
   def project do
     [
       app: :td_cache,
-      version: "6.15.2",
+      version: "7.0.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
